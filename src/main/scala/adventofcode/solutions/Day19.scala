31dfe3ea21a32aae6a20b5b14f101b87540f6955
Florian Cassayre <florian.cassayre@gmail.com>
2023-01-01 16:18:42 +0100

    Improved day 19

--- src/main/scala/adventofcode/solutions/Day19.scala
+++ src/main/scala/adventofcode/solutions/Day19.scala
@@ -27,54 +27,53 @@ import adventofcode.Definitions.*
       Blueprint(id.toInt, oreCostOre.toInt, clayCostOre.toInt, obsidianCostOre.toInt, obsidianCostClay.toInt, geodeCostOre.toInt, geodeCostObsidian.toInt)
   }
 
-  def solve(states: Set[(Map[Material, Int], Map[Material, Int])], time: Int, costs: Map[Material, Map[Material, Int]]): Int =
+  def solve(states: Set[(Map[Material, Int], Map[Material, Int], Set[Material])], time: Int, costs: Map[Material, Map[Material, Int]]): Int =
     if time > 0 then
+      val maxRobots = costs.values.toSeq.flatMap(_.toSeq).groupBy(_._1).view.mapValues(_.map(_._2).max).toMap
       val geodesLowerBound = states.map(t => t._1(Geode) + t._2(Geode) * time).maxOption.getOrElse(0)
-      val filteredMaxObtainable = states.filter { case (resources, robots) =>
+      val filteredMaxObtainable = states.filter { case (resources, robots, _) =>
         val currentRobots = robots(Geode)
         val maxObtainableGeodes = resources(Geode) + time * currentRobots + time * (time + 1) / 2
         maxObtainableGeodes > geodesLowerBound
       }
-      val filteredSuboptimal = filteredMaxObtainable.groupBy(_._2).view.mapValues { values =>
+      val filteredSuboptimal = filteredMaxObtainable.groupBy(t => (t._2, t._3)).view.mapValues { values =>
         val resources = values.map(_._1)
         resources.filter(r1 => !resources.exists(r2 => r1 != r2 && r2.forall(t => r1(t._1) <= t._2)))
-      }.flatMap(t => t._2.map(_ -> t._1)).toSet
-      val filteredZeroed = time match
-        case 3 => filteredSuboptimal.filter(t => t._2(Clay) > 0 || t._2(Obsidian) > 0 || t._2(Geode) > 0)
-        case 2 => filteredSuboptimal.filter(t => t._2(Obsidian) > 0 || t._2(Geode) > 0)
-        case 1 => filteredSuboptimal.filter(_._2(Geode) > 0)
-        case _ => filteredSuboptimal
+      }.flatMap(t => t._2.map(v => (v, t._1._1, t._1._2))).toSet
 
-      val newStates = filteredZeroed.flatMap { case (resources, robots) =>
-        val newResourcesAndRobotsAfterPurchase = materials
-          .filter(material => costs(material).forall { case (material, amount) => resources(material) >= amount })
+      val newStates = filteredSuboptimal.flatMap { case (resources, robots, refused) =>
+        val purchasableRobots = materials
+          .filter(material => !refused.contains(material) && costs(material).forall { case (material, amount) => resources(material) >= amount })
+        val newResourcesAndRobotsAfterPurchase = purchasableRobots
           .map(material =>
             (
               costs(material).foldLeft(resources) { case (acc, (material, amount)) => acc + (material -> (acc(material) - amount)) },
-              robots + (material -> (robots(material) + 1))
+              robots + (material -> (robots(material) + 1)),
+              Set.empty[Material]
             )
           )
+        val actualNewResourcesAndRobotsAfterPurchase = newResourcesAndRobotsAfterPurchase.filter { case (_, newRobots, _) =>
+          newRobots.forall { case (material, quantity) => !maxRobots.contains(material) || quantity <= maxRobots(material) }
+        }
         val couldPurchase = costs.values.count(cost => cost.keys.forall(robots.contains))
-        val newResourcesAndRobots = if newResourcesAndRobotsAfterPurchase.size < couldPurchase then newResourcesAndRobotsAfterPurchase :+ (resources, robots) else newResourcesAndRobotsAfterPurchase
-        newResourcesAndRobots.map { case (newResources, newRobots) =>
+        val newResourcesAndRobots =
+          if newResourcesAndRobotsAfterPurchase.size < couldPurchase then
+            actualNewResourcesAndRobotsAfterPurchase :+ (resources, robots, refused ++ purchasableRobots.toSet)
+          else
+            actualNewResourcesAndRobotsAfterPurchase
+        newResourcesAndRobots.map { case (newResources, newRobots, newRefused) =>
           val newResourcesAfterCollection = robots.foldLeft(newResources) { case (acc, (material, amount)) => acc + (material -> (acc(material) + amount)) }
-          (newResourcesAfterCollection, newRobots)
+          (newResourcesAfterCollection, newRobots, newRefused)
         }
       }
       solve(newStates, time - 1, costs)
     else
       states.map(_._1(Geode)).maxOption.getOrElse(0)
 
-  lazy val part1 = blueprints.map(blueprint => blueprint.id *
-    solve(Set((emptyMaterials, emptyMaterials + (Ore -> 1))), 24, blueprint.costs)
-  ).sum
+  val initialState = Set((emptyMaterials, emptyMaterials + (Ore -> 1), Set.empty[Material]))
 
-  part(1) = 1616
+  part(1) = blueprints.map(blueprint => blueprint.id * solve(initialState, 24, blueprint.costs)).sum
 
-  lazy val part2 = blueprints.take(3).map(blueprint =>
-    solve(Set((emptyMaterials, emptyMaterials + (Ore -> 1))), 32, blueprint.costs)
-  ).product
-
-  part(2) = 8990
+  part(2) = blueprints.take(3).map(blueprint => solve(initialState, 32, blueprint.costs)).product
 
 }
